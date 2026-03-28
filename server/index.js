ce2d95dacc4425f3f997bdbcdcc04bfe1c897fee
Tejaswini074 <tejaswinis532@gmail.com>
2026-03-28 18:23:09 +0530

    removed inside folder dockerfile

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ app.use(cors({ origin: "*", credentials: true }));
 app.get("/api/msg", (req, res) => {
   res.json({ message: "Hello from VS code 🚀" });
 });
-
+app.get("/api/test", (req, res) => {
+  res.send("API working");
+});
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {

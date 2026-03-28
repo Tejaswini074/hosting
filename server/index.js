8a81616d79b101de80a7b5dc9df173bab93b32ef
Tejaswini074 <tejaswinis532@gmail.com>
2026-03-28 18:19:20 +0530

    final

--- server/index.js
+++ server/index.js
@@ -1,36 +1,16 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({ origin: "*", credentials: true }));
 
-
 app.get("/api/msg", (req, res) => {
   res.json({ message: "Hello from VS code 🚀" });
 });
-
-
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong" });
-});
-
 
 const PORT = process.env.PORT || 4000;
 
